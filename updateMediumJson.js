a500b2c257f6751462acf513d80c13a8fcc06528
iquidus <luke@iquidus.io>
2021-05-27 13:29:56 +0200

    add updateMediumJson.js (fetch blog posts via rss and write to json)

---
+++ updateMediumJson.js
@@ -0,0 +1,29 @@
+const Parser = require('rss-parser')
+const jsonfile = require('jsonfile')
+const parser = new Parser()
+
+const file = 'assets/medium.json'
+
+const update = async () => {
+  const feed = await parser.parseURL('https://medium.com/feed/the-ubiq-report')
+  console.log(feed.title)
+
+  // extract img src from content:encoded and set as item.image
+  // why you make me do this? -_-
+  for (const item of feed.items) {
+    const encoded = item['content:encoded']
+    const start = encoded.indexOf('<img ')
+    const end = encoded.indexOf('" />', start)
+    const img = encoded.substr(start + 17, end - start - 17)
+    item.image = img
+  }
+
+  jsonfile
+    .writeFile(file, feed.items, { spaces: 2 })
+    .then((res) => {
+      console.log('Write complete')
+    })
+    .catch((error) => console.error(error))
+}
+
+update()
