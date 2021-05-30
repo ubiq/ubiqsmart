d91512f769510b011803e4f265f7983b404715be
iquidus <luke@iquidus.io>
2021-05-31 00:32:09 +0200

    Market: change card layout if mobile

--- components/Market.vue
+++ components/Market.vue
@@ -1,19 +1,29 @@
 <template>
   <v-card flat outlined tile class="mt-4 mb-6">
-    <div class="d-flex flex-no-wrap justify-space-between">
+    <template v-if="!isMobile">
+      <div class="d-flex flex-no-wrap justify-space-between">
+        <v-avatar class="ma-3" size="125" tile>
+          <v-img :src="'markets/' + marketId + '.png'" />
+        </v-avatar>
+        <div>
+          <v-card-subtitle>
+            <slot></slot>
+          </v-card-subtitle>
+        </div>
+      </div>
+    </template>
+    <template v-else>
       <v-img
         :src="'markets/' + marketId + '.png'"
+        height="125"
+        position="top center"
+        class="mt-4"
         contain
-        height="128px"
-        width="128px"
-        class="my-6 ml-6"
-      ></v-img>
-      <v-container>
-        <v-card-text>
-          <slot></slot>
-        </v-card-text>
-      </v-container>
-    </div>
+      />
+      <v-card-subtitle>
+        <slot></slot>
+      </v-card-subtitle>
+    </template>
     <v-card-actions class="bt-1 pa-0">
       <v-btn
         :href="markets[marketId].url"
@@ -44,6 +54,9 @@ export default {
   computed: {
     markets() {
       return Markets
+    },
+    isMobile() {
+      return this.$store.state.mobile
     },
   },
 }
