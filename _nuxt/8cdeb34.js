c18e0069620118c9a2c42fed644204abdc7e64ec
iquidus <iquidus@users.noreply.github.com>
2022-01-02 22:30:21 +0000

    deploy: 3694e77a18ab019578733380fa082df9f4f7da1b

---
+++ _nuxt/8cdeb34.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{535:function(e,n,t){"use strict";t.r(n);var l={name:"ExampleComponent"},o=t(42),component=Object(o.a)(l,(function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("v-card",{attrs:{flat:""}},[t("v-card-title",[e._v("Example Component")])],1)}),[],!1,null,null,null);n.default=component.exports}}]);
