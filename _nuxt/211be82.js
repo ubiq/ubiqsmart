71e59563fea9644b0f5b4c28c4a069c4875aec1f
iquidus <iquidus@users.noreply.github.com>
2023-09-13 20:51:48 +0000

    deploy: 63b50b0e506059a052068d754aa9428f34f636b3

--- _nuxt/211be82.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{585:function(n,t,e){"use strict";e.r(t);var l={name:"ExampleComponent"},o=e(42),component=Object(o.a)(l,(function(){var n=this._self._c;return n("v-card",{attrs:{flat:""}},[n("v-card-title",[this._v("Example Component")])],1)}),[],!1,null,null,null);t.default=component.exports}}]);
